b978489bc59eedbc48af6d85c0f0afd8ea105f1b
Santosh P katageri <santoshpkatageri@users.noreply.github.com>
2017-08-28 15:24:57 +0530

    Update mysqldb.sh

--- mysqldb.sh
+++ mysqldb.sh
@@ -16,19 +16,19 @@ echo "mysql-server-5.6 mysql-server/root_password_again password $mysqlPassword"
 sudo apt-get -y install mysql-server
 
 #setting password for root user in DB host
-sudo mysql -u root -e "set password for 'root'@'192.168.10.10' = PASSWORD('$mysqlPassword')"
+sudo mysql -u root -e "set password for 'root'@'192.168.20.20' = PASSWORD('$mysqlPassword')"
 
 #changing Bind address to LAP VM address
-sudo sed -i "s/.*bind-address.*/bind-address = 192.168.10.10/" /etc/mysql/my.cnf
+sudo sed -i "s/.*bind-address.*/bind-address = 192.168.20.20/" /etc/mysql/my.cnf
 
 #creating database for wordpress
 sudo mysql -u root -p${mysqlPassword}  -e "CREATE DATABASE ${db};"
 
 #setting password for remote login from LAP vm
-#sudo mysql -u root -e "set password for 'santosh'@'10.0.0.7' = PASSWORD('$mysqlPassword')"
+sudo mysql -u root -e "set password for 'santosh'@'192.168.10.10' = PASSWORD('$mysqlPassword')"
 
 #Granting permision to remote login
-#sudo mysql -u root -p${mysqlPassword} -e "GRANT ALL PRIVILEGES ON ${db}.* TO santosh@'10.0.0.7' IDENTIFIED BY '${mysqlPassword}';"
+sudo mysql -u root -p${mysqlPassword} -e "GRANT ALL PRIVILEGES ON ${db}.* TO santosh@'192.168.10.10' IDENTIFIED BY '${mysqlPassword}';"
 
 #Restarting the mysql server
 sudo service mysql restart
